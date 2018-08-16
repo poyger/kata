f4265aa342124751126d386c58e3d2015f721535
Gerami <poyan.gerami@hm.com>
2018-08-16 09:52:34 +0200

    added comment for visitor pattern

--- java/pattern/visitor/Client.java
+++ java/pattern/visitor/Client.java
@@ -2,6 +2,10 @@ package pattern.visitor;
 
 import java.util.List;
 
+/**
+ * kata for testing visitor pattern, visitor can be used for detaching functionality outside the domain class, in this case
+ * we counting the price for Fruit and Book with a price counter visitor.
+ */
 public class Client {
     public static void main(String[] args) {
         final List<Product> products = List.of(new Book(100, "Utvandrarna"), new Fruit(10, "Apelsiner", 2));
